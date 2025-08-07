e9110995de63413714a515e71da480d7a8274cd8
tam11a <ibrahimsadiktamim@gmail.com>
2025-08-07 21:07:07 +0600

    signup submission

--- src/app/(auth)/signup/action.ts
+++ src/app/(auth)/signup/action.ts
@@ -1,16 +1,31 @@
 "use server";
 
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+import { redirect, RedirectType } from "next/navigation";
+
+const prisma = new PrismaClient();
+
 export async function signup(formData: FormData) {
   "use server";
 
-  const name = formData.get("name") as string;
+  const full_name = formData.get("full_name") as string;
   const phone = formData.get("phone") as string;
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
 
-  // Here you would typically send the data to your backend API
-  // For demonstration, we will just log it and return a success response
-  console.log({ name, phone, email, password });
-
-  //   return { success: true, message: "Signup successful!" };
+  try {
+    await prisma.user.create({
+      data: {
+        full_name,
+        phone,
+        email,
+        password: await bcrypt.hash(password, 10), // Hash the password
+      },
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    throw new Error("Failed to create user. Please try again.");
+  }
+  redirect("/login", RedirectType.replace); // Redirect to login page after successful signup
 }
