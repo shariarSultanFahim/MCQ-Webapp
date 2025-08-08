4a2a22e4a5722d1076111bebc3e089372704603f
shariarsultanfahim <shariarsfahim@gmail.com>
2025-08-08 06:32:02 +0600

    Linked landing page to home page route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
-import { Button, Stack } from "@mui/material";
+import HomePage from "./(landing)/home/page";
 
 export default function Home() {
-  return (
-    <Stack spacing={2} direction="column" alignItems="center" className="my-20">
-      <Button variant="contained" color="primary">
-        Click Me
-      </Button>
-      <Button variant="outlined" color="primary">
-        Click Me
-      </Button>
-      <Button variant="text" color="primary">
-        Click Me
-      </Button>
-    </Stack>
-  );
+  return <HomePage />;
 }
